a2578e813d5a99680b71846868a66069ab57355a
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-30 15:12:02 -0500

    corrected extension from tsx to jsx on error page import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,8 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/HomePage.jsx'
-import ErrorPage from './pages/ErrorPage.tsx'
+// import ErrorPage from './pages/ErrorPage.tsx'
+import ErrorPage from './pages/ErrorPage.jsx';
 
 
 const router = createBrowserRouter([
