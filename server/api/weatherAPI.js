aabdf012ca0c244f7fa9f246d22e5b30a11fe512
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-30 15:08:53 -0500

    updated with console logs to debug code and imports to try and get env working, which have been commented out for now

--- server/api/weatherAPI.js
+++ server/api/weatherAPI.js
@@ -1,42 +1,46 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+
 const getLatLon = async (zipcode) => {
     try {
-      const latLonResponse = await fetch(
-        `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode}&appid=
+        console.log('Weather API Key = ', import.meta.env.WEATHER_API_KEY);
+        const latLonResponse = await fetch(
+            `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode}&appid=
         ${import.meta.env.WEATHER_API_KEY}`
-      );
-    //   console.log('latLonResponse:', latLonResponse);
-      const latLonData = await latLonResponse.json();
-      if (!latLonResponse.ok) {
-        throw new Error('invalid API response, check the network tab');
-      }
-    //   console.log('latLonData:', latLonData);
-      return latLonData;
+        );
+        console.log('latLonResponse:', latLonResponse);
+        const latLonData = await latLonResponse.json();
+        if (!latLonResponse.ok) {
+            throw new Error('invalid API response, check the network tab');
+        }
+        console.log('latLonData:', latLonData);
+        return latLonData;
     } catch (err) {
-      console.log('an error occurred', err);
-      return [];
+        console.log('an error occurred', err);
+        return [];
     }
-  };
+};
 
-  const getWeatherConditions = async (lat, lon) => {
+const getWeatherConditions = async (lat, lon) => {
     try {
         const conditionsResponse = await fetch(
             `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&cnt=1&appid=
             ${import.meta.env.WEATHER_API_KEY}`
         );
-      //   console.log('conditionsResponse:', conditionsResponse);
+        //   console.log('conditionsResponse:', conditionsResponse);
         const conditionsData = await conditionsResponse.json();
         if (!conditionsResponse.ok) {
-          throw new Error('invalid API response, check the network tab');
+            throw new Error('invalid API response, check the network tab');
         }
-      //   console.log('conditionsData:', conditionsData);
+        //   console.log('conditionsData:', conditionsData);
         return conditionsData;
-      } catch (err) {
+    } catch (err) {
         console.log('an error occurred', err);
         return [];
-      }
-  };
-  
-  export { getLatLon, getWeatherConditions };
+    }
+};
+
+export { getLatLon, getWeatherConditions };
 
 //   NESTING TWO FUNCTIONS IS AN OPTION
 //   const getLatLon = async (zipcode) => {
@@ -57,4 +61,3 @@ const getLatLon = async (zipcode) => {
 //       return [];
 //     }
 //   };
-  
