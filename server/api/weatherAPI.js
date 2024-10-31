8803406a0ab1589fd69b72ec7cb40a9fd55ca3a7
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-31 11:11:25 -0500

    removed testing imports and changed import.meta to process on all env key calls

--- server/api/weatherAPI.js
+++ server/api/weatherAPI.js
@@ -1,12 +1,11 @@
-// import dotenv from 'dotenv';
-// dotenv.config();
 
 const getLatLon = async (zipcode) => {
+    console.log('Weather API Key = ', process.env.WEATHER_API_KEY);
     try {
-        console.log('Weather API Key = ', import.meta.env.WEATHER_API_KEY);
+        
         const latLonResponse = await fetch(
             `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode}&appid=
-        ${import.meta.env.WEATHER_API_KEY}`
+        ${process.env.WEATHER_API_KEY}`
         );
         console.log('latLonResponse:', latLonResponse);
         const latLonData = await latLonResponse.json();
@@ -22,10 +21,11 @@ const getLatLon = async (zipcode) => {
 };
 
 const getWeatherConditions = async (lat, lon) => {
+    console.log('Weather API Key = ', process.env.WEATHER_API_KEY);
     try {
         const conditionsResponse = await fetch(
             `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&cnt=1&appid=
-            ${import.meta.env.WEATHER_API_KEY}`
+            ${process.env.WEATHER_API_KEY}`
         );
         //   console.log('conditionsResponse:', conditionsResponse);
         const conditionsData = await conditionsResponse.json();
@@ -41,23 +41,3 @@ const getWeatherConditions = async (lat, lon) => {
 };
 
 export { getLatLon, getWeatherConditions };
-
-//   NESTING TWO FUNCTIONS IS AN OPTION
-//   const getLatLon = async (zipcode) => {
-//     try {
-//       const latLonResponse = await fetch(
-//         `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode}&appid=
-//         ${import.meta.env.WEATHER_API_KEY}`
-//       );
-//     //   console.log('latLonResponse:', latLonResponse);
-//       const latLonData = await latLonResponse.json();
-//       if (!latLonResponse.ok) {
-//         throw new Error('invalid API response, check the network tab');
-//       }
-//     //   console.log('latLonData:', latLonData);
-//       return latLonData;
-//     } catch (err) {
-//       console.log('an error occurred', err);
-//       return [];
-//     }
-//   };
