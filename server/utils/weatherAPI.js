afdce46b1f06fd1b1781b709e1d925559fd82e8d
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-01 12:39:24 -0500

    changed url in getWeatherConditions to be one line, commented out console logs

--- server/utils/weatherAPI.js
+++ server/utils/weatherAPI.js
@@ -1,16 +1,16 @@
 const getLatLon = async (zipcode) => {
-    console.log('Weather API Key = ', process.env.WEATHER_API_KEY);
+    // console.log('Weather API Key = ', process.env.WEATHER_API_KEY);
     try {
         
         const latLonResponse = await fetch(
             `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode}&appid=${process.env.WEATHER_API_KEY}`
         );
-        console.log('latLonResponse:', latLonResponse);
+        // console.log('latLonResponse:', latLonResponse);
         const latLonData = await latLonResponse.json();
         if (!latLonResponse.ok) {
             throw new Error('invalid API response, check the network tab');
         }
-        console.log('latLonData:', latLonData);
+        // console.log('latLonData:', latLonData);
         return latLonData;
     } catch (err) {
         console.log('an error occurred', err);
@@ -19,11 +19,10 @@ const getLatLon = async (zipcode) => {
 };
 
 const getWeatherConditions = async (lat, lon) => {
-    console.log('Weather API Key = ', process.env.WEATHER_API_KEY);
+    // console.log('Weather API Key = ', process.env.WEATHER_API_KEY);
     try {
         const conditionsResponse = await fetch(
-            `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&cnt=1&appid=
-            ${process.env.WEATHER_API_KEY}`
+            `http://api.openweathermap.org/data/2.5/forecast?cnt=1&lat=${lat}&lon=${lon}&units=imperial&appid=${process.env.WEATHER_API_KEY}`
         );
         //   console.log('conditionsResponse:', conditionsResponse);
         const conditionsData = await conditionsResponse.json();
