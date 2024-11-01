ee672ffb2705997d750cce99647d5143f0481e68
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-01 15:17:14 -0500

    got rid of non-functioning code and made comments explaining the situation and to-do's

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
-//import pg from 'pg';
-require("dotenv").config(); 
+//import pg from 'pg'; // do we still need this?
+require("dotenv").config();
+
 const weatherAPI = require('./utils/weatherAPI');
 const spotifyAPI = require('./utils/spotifyAPI');
+
 const cors = require('cors');
-
 const request = require('request');
-
 const express = require('express');
 // Import and require Pool (node-postgres)
-// We'll be creating a Connection Pool. Read up on the benefits here: https://node-postgres.com/features/pooling
 const { Pool } = require('pg');
 
 const PORT = process.env.PORT || 3001;
@@ -32,26 +31,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+// TO-DO: consider renaming this end-point (route)
 app.get('/api/weather', async (req, res) => {
   try {
-    // console.log(req);
-          // get the lat and lon
-      const weatherData = await weatherAPI.getLatLon(req.query.zip); // Assuming you have a function to fetch data
-      console.log(weatherData);
-      // console.log(weatherData);
-      // pass in the lat and lon in the request to get the weather conditions
-      const weatherData2 = await weatherAPI.getWeatherConditions(weatherData.lat, weatherData.lon);
-      
-      console.log(weatherData2);
-      // making another req to spotify using the weather conditions from query
-      const spotifyData = await spotifyAPI.getPlaylists('clouds', 'BQB8Fe_H6rc7jiqg0aDcvuJ5aMQ3eqcVRsn2Xx8smL-eTOaa83lcqDuXf5uUwYKPJAESezfGDSzI-FQ6pyKTbait_hsdiLJYWAM79O7cBWd-ogNDxK8');
-
-      console.log(spotifyData);
-      // then, returning the playlists from spotify
-      // then, updating the database with the weather conditions and the playlist
-      res.json(weatherData);
+    // get the lat and lon
+    const latLonData = await weatherAPI.getLatLon(req.query.zip); // get the object that contains the lat and lon data
+    // console.log(latLonData);
+    // pass in the lat and lon in the request to get the weather conditions
+    const conditionsData = await weatherAPI.getWeatherConditions(latLonData.lat, latLonData.lon);
+    // console.log(conditionsData);
+    // console.log('conditionsData description: ', conditionsData.list[0].weather[0].description); // conditions keywords
+    // TO-DO: NEED TO GET RID OF THE FOLLOWING LINES RELATED TO TOKEN AND MAKE THEM ACTUALLY WORK AND RETURN A TOKEN FOR GETPLAYLISTS FUNCTION TO USE
+    // const token = await spotifyAPI.spotifyToken();
+    // token.then((i) => { return i });
+    // console.log('token: ', token);
+    // then, returning the playlists from spotify
+    const spotifyData = await spotifyAPI.getPlaylists(conditionsData.list[0].weather[0].description, token); // currently token does not work and needs to be hard-coded
+    // console.log(spotifyData);
+    // then, update the database with the weather conditions and the playlist (maybe?? let's talk about this)
+    // TO-DO
+    res.json(weatherData);
   } catch (error) {
-      res.status(500).json({ error: 'Failed to fetch weather data' });
+    res.status(500).json({ error: 'Failed to fetch weather data' });
   }
 });
 
@@ -89,7 +90,7 @@ app.get('/api/locations', (req, res) => {
   });
 });
 
-app.get('/refresh_token', function(req, res) {
+app.get('/refresh_token', function (req, res) {
 
   var refresh_token = "req.query.refresh_token";
   var authOptions = {
@@ -104,20 +105,17 @@ app.get('/refresh_token', function(req, res) {
     },
     json: true
   };
-
-  // console.log(authOptions);
-  request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function (error, response, body) {
     console.log('error: ', error);
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token,
-          refresh_token = body.refresh_token || refresh_token;
+        refresh_token = body.refresh_token || refresh_token;
       res.send({
         'access_token': access_token,
         'refresh_token': refresh_token
       });
     }
   });
-// res.json({});
 });
 
 // Delete Request
