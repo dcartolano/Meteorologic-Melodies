67f3dbb959fecdb959c0e04db06d132dfafbcdc0
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-31 19:03:28 -0500

    initial commit

---
+++ client/src/components/PlaylistCard.jsx
@@ -0,0 +1,11 @@
+
+
+function PlaylistCard() {
+    return (
+        <div className="">
+            <h3 className="">`Recommended playlists go here...`</h3>
+        </div>
+    );
+}
+
+export default PlaylistCard;
---
+++ client/src/components/RecentSearches.jsx
@@ -0,0 +1,10 @@
+
+function RecentSearches() {
+    return (
+        <div className="">
+            <h5 className="">`Recent searches go here...`</h5>
+        </div>
+    );
+}
+
+export default RecentSearches;
