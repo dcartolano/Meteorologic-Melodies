3083c005425dc1eb24706362b0a54dd8fbb6601f
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-01 12:49:45 -0500

    added a lot of test and checking code while trying to get this functional, though still not quite there

--- server/utils/spotifyAPI.js
+++ server/utils/spotifyAPI.js
@@ -1,16 +1,21 @@
-const getPlaylists = async (query) => {
+const getPlaylists = async (condition, token) => {
     try {
         
         // console.log(import.meta.env);
-        const response = await fetch(
-        `https://api.spotify.com/v1/search?q=${query}&type=playlist&limit=5`,
+        console.log('condition:',condition);
+        const response = await fetch(`http://api.spotify.com/v1/search?q=${condition}&type=playlist&limit=5/`,
+        // `http://api.spotify.com/v1/search?q=cloudy&type=playlist&limit=5`,
         {
+            method: 'GET',
+            // mode: 'no-cors',
             headers: {
-            Authorization: `Bearer ${import.meta.env.SPOTIFY_API_TOKEN}`,
+            // 'Authorization': `Bearer ${token}`,
+            'Authorization': `Bearer BQB8Fe_H6rc7jiqg0aDcvuJ5aMQ3eqcVRsn2Xx8smL-eTOaa83lcqDuXf5uUwYKPJAESezfGDSzI-FQ6pyKTbait_hsdiLJYWAM79O7cBWd-ogNDxK8`,
+            'Content-Type': 'application/json',
             },
         }
         );
-        // console.log('Response:', response);
+        console.log('Response:', response);
         const data = await response.json();
         if (!response.ok) {
         throw new Error('invalid API response, check the network tab');
@@ -18,10 +23,11 @@ const getPlaylists = async (query) => {
         // console.log('Data:', data);
         return data;
     } catch (err) {
-        // console.log('an error occurred', err);
+        console.log('an error occurred', err);
         return [];
     }
   };
   
-  export { getPlaylists };
+//   export { getPlaylists };
+  module.exports= {getPlaylists};
   
