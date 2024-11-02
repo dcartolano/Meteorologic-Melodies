99fb6497fbb8f576aac78fc731fdfdf64024a720
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-01 19:16:27 -0500

    took out code that was no longer needed, commented out console logs that were no longer needed

--- server/utils/spotifyAPI.js
+++ server/utils/spotifyAPI.js
@@ -2,9 +2,8 @@ const request = require('request');
 
 const getPlaylists = async (condition, token) => {
     try {
-        // console.log(import.meta.env);
-        console.log('condition:', condition);
-        console.log('token:', token);
+        // console.log('condition:', condition);
+        // console.log('token:', token);
         const response = await fetch(`https://api.spotify.com/v1/search?q=${condition}&type=playlist&limit=5`,
             {
                 headers: {
@@ -25,88 +24,4 @@ const getPlaylists = async (condition, token) => {
     }
 };
 
-// probably will need to delete all of this commented out code, as it is not functioning properly
-// supposedly, we need to keep the route in the server.js file, and make a request to that end point within the other request and take the token from there
-// app.get('/refresh_token', function(req, res) {
-
-//     var refresh_token = "req.query.refresh_token";
-//     var authOptions = {
-//       url: 'https://accounts.spotify.com/api/token',
-//       headers: {
-//         'content-type': 'application/x-www-form-urlencoded',
-//         'Authorization': 'Basic ' + (new Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
-//       },
-//       form: {
-//         grant_type: 'client_credentials',
-//         refresh_token: refresh_token
-//       },
-//       json: true
-//     };
-  
-//     // console.log(authOptions);
-//     request.post(authOptions, function(error, response, body) {
-//       console.log('error: ', error);
-//       if (!error && response.statusCode === 200) {
-//         var access_token = body.access_token,
-//             refresh_token = body.refresh_token || refresh_token;
-//         res.send({
-//           'access_token': access_token,
-//           'refresh_token': refresh_token
-//         });
-//       }
-//     });
-//   // res.json({});
-//   });
-
-// const spotifyToken = async () => {
-
-//     let refresh_token = "req.query.refresh_token";
-//     let authOptions = {
-//         url: 'https://accounts.spotify.com/api/token',
-//         headers: {
-//             'content-type': 'application/x-www-form-urlencoded',
-//             'Authorization': 'Basic ' + (new Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
-//         },
-//         form: {
-//             grant_type: 'client_credentials',
-//             refresh_token: refresh_token
-//         },
-//         json: true
-//     };
-
-// // console.log(authOptions);
-// request.post(authOptions, function(error, response, body) {
-//     console.log('error: ', error);
-//     if (!error && response.statusCode === 200) {
-//       var access_token = body.access_token,
-//           refresh_token = body.refresh_token || refresh_token;
-//       return ({
-//         'access_token': access_token,
-//         'refresh_token': refresh_token
-//       });
-//     }
-//   });
-
-//     const token =  request.post(authOptions, function (error, response, body) {
-//         console.log('error: ', error);
-//         //   console.log('response: ',response);
-//         //   console.log('body: ', body);
-//         // return body.access_token;
-//         // console.log(response.body.access_token);
-//             return body.access_token;
-//             // if (!error && response.statusCode === 200) {
-//             //   var access_token = body.access_token,
-//             //       refresh_token = body.refresh_token || refresh_token;
-//             // //   return ({
-//             // //     'access_token': access_token,
-//             // //     'refresh_token': refresh_token
-//             // //   });
-            
-//             // }
-//           });
-//           return token;
-//         };
-    
-//   export { getPlaylists };
-// module.exports = { getPlaylists, spotifyToken };
 module.exports = { getPlaylists };
