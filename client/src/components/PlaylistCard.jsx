f961179ff9855f36dbf50705a627054c6c2b584f
5mitty <43888178+5mitty@users.noreply.github.com>
2024-11-02 17:45:39 -0500

    created html and used passed props in playlist card

--- client/src/components/PlaylistCard.jsx
+++ client/src/components/PlaylistCard.jsx
@@ -1,11 +1,42 @@
 
 
-function PlaylistCard() {
+function PlaylistCard({PlaylistName, PlaylistUrl}) {
+    // console.log('playlist name: ', PlaylistName);
     return (
         <div className="">
-            <h3 className="">Recommended playlists go here...</h3>
+            <div>
+                <h3 className="">
+                    {`Spotify Playlist: `}
+                </h3>
+            </div>
+            <div>
+                <a className="" href={PlaylistUrl} target="_blank">
+                    {`Click here for ${PlaylistName}`}
+                </a>
+            </div>
+            {/* <div>
+                <h3 className="">
+                    {`${Playlists.playlists.items[0].owner.display_name}`}
+                </h3>
+            </div>
+            <div>
+                <h3 className="">
+                    {`${Playlists.playlists.items[0].owner.external_urls.spotify}`}
+                </h3>
+            </div> */}
         </div>
+        
     );
 }
 
 export default PlaylistCard;
+
+
+
+
+
+
+
+
+
+// <a><img src={Playlists/playlists.items[0].images[0].url} alt="Playlist Image"></img></a>
