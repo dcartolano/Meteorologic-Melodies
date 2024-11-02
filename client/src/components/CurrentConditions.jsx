57d579cacb515a88552a7c960a4c2a398c362866
5mitty <43888178+5mitty@users.noreply.github.com>
2024-11-02 15:54:37 -0500

    recieves Conditions from HomePage.jsx

--- client/src/components/CurrentConditions.jsx
+++ client/src/components/CurrentConditions.jsx
@@ -1,11 +1,25 @@
 
 
-function CurrentConditions() {
+function CurrentConditions({Conditions = ""}) {
+    console.log(Conditions);
     return (
         <div className="">
-            <h3 className="">Your current conditions are...</h3>
+            <h3 className="">
+                {`Your current conditions are... ${Conditions}`}
+            </h3>
         </div>
     );
 }
 
 export default CurrentConditions;
+
+// const ChildComponent = ({ data }) => {
+//     return (
+//         <div>
+//             <h2>Child Component</h2>
+//             <p>Name: {data.name}</p>
+//             <p>Age: {data.age}</p>
+//             <p>Occupation: {data.occupation}</p>
+//         </div>
+//     );
+// };
