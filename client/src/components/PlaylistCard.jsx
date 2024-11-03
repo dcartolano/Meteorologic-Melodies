347aa22d517e83c1254e6c66c63171d630f70e55
5mitty <43888178+5mitty@users.noreply.github.com>
2024-11-03 13:51:48 -0600

    added link functionality to PlaylistPicture, PlaylistName, and PlaylistUrl. Added PlayerOwnerName and PlayerOwnerLink with functionality.

--- client/src/components/PlaylistCard.jsx
+++ client/src/components/PlaylistCard.jsx
@@ -1,35 +1,35 @@
 // import { Link } from 'react-router-dom';
 
-function PlaylistCard({PlaylistName, PlaylistUrl, PlaylistPicture}) {
+function PlaylistCard({PlaylistName, PlaylistUrl, PlaylistPicture, PlaylistOwnerName, PlaylistOwnerLink}) {
     // console.log('playlist name: ', PlaylistName);
     return (
-        <a href={PlaylistUrl} target="_blank">
+        // <a href={PlaylistUrl} target="_blank">
             <div className="">
                 {/* <a> */}
                     <img src={PlaylistPicture} alt="Playlist Image"></img>
                 {/* </a> */}
                 <div>
-                    <h3 className="">
-                        {`Spotify Playlist: `}
-                    </h3>
+                    <a className="" href={PlaylistUrl} target="_blank">
+                        {`Spotify Playlist: ${PlaylistUrl}`}
+                    </a>
                 </div>
                 <div>
-                    {/* <a className="" > */}
+                    <a className="" href={PlaylistUrl} target="_blank">
                         {`Click here for ${PlaylistName}`}
-                    {/* </a> */}
-                </div>
-                {/* <div>
-                    <h3 className="">
-                        {`${Playlists.playlists.items[0].owner.display_name}`}
-                    </h3>
+                    </a>
                 </div>
                 <div>
                     <h3 className="">
-                        {`${Playlists.playlists.items[0].owner.external_urls.spotify}`}
+                        {`Playlist Owner Name: ${PlaylistOwnerName}`}
                     </h3>
-                </div> */}
+                </div>
+                <div>
+                    <a className=""href={PlaylistOwnerLink} target="_blank" >
+                        {`Playlist Owner: ${PlaylistOwnerLink}`}
+                    </a>
+                </div>
             </div>
-        </a>
+        // </a>
     );
 }
 
