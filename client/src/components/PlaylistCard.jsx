6fc730a4ed93b0d6ebe6ddbe2c47a1861100cfe1
5mitty <43888178+5mitty@users.noreply.github.com>
2024-11-02 19:08:49 -0500

    passed PlaylistPicture prop and added playlist picture in PlaylistCard html

--- client/src/components/PlaylistCard.jsx
+++ client/src/components/PlaylistCard.jsx
@@ -1,35 +1,41 @@
-function PlaylistCard({PlaylistName, PlaylistUrl}) {
+// import { Link } from 'react-router-dom';
+
+function PlaylistCard({PlaylistName, PlaylistUrl, PlaylistPicture}) {
     // console.log('playlist name: ', PlaylistName);
     return (
-        <div className="">
-            <div>
-                <h3 className="">
-                    {`Spotify Playlist: `}
-                </h3>
-            </div>
-            <div>
-                <a className="" href={PlaylistUrl} target="_blank">
-                    {`Click here for ${PlaylistName}`}
+        <a href={PlaylistUrl} target="_blank">
+            <div className="">
+                <a>
+                    <img src={PlaylistPicture} alt="Playlist Image"></img>
                 </a>
+                <div>
+                    <h3 className="">
+                        {`Spotify Playlist: `}
+                    </h3>
+                </div>
+                <div>
+                    <a className="" >
+                        {`Click here for ${PlaylistName}`}
+                    </a>
+                </div>
+                {/* <div>
+                    <h3 className="">
+                        {`${Playlists.playlists.items[0].owner.display_name}`}
+                    </h3>
+                </div>
+                <div>
+                    <h3 className="">
+                        {`${Playlists.playlists.items[0].owner.external_urls.spotify}`}
+                    </h3>
+                </div> */}
             </div>
-            {/* <div>
-                <h3 className="">
-                    {`${Playlists.playlists.items[0].owner.display_name}`}
-                </h3>
-            </div>
-            <div>
-                <h3 className="">
-                    {`${Playlists.playlists.items[0].owner.external_urls.spotify}`}
-                </h3>
-            </div> */}
-        </div>
-        
+        </a>
     );
 }
 
 export default PlaylistCard;
 
-
+// href={PlaylistUrl} target="_blank"
 
 
 
