610440a7b29a7734c717ff739588c8d3bdc035c2
5mitty <43888178+5mitty@users.noreply.github.com>
2024-11-03 19:00:50 -0600

    added className for spotifyPlaylistCard, spotifyPlaylistMiniCard, spotifyPlaylistPicture, PlaylistTextDiv, PlaylistText. changed around the structure of how links and text are being displayed to make the cards more concise, removed unnessary details and polishing text.

--- client/src/components/PlaylistCard.jsx
+++ client/src/components/PlaylistCard.jsx
@@ -1,32 +1,27 @@
 // import { Link } from 'react-router-dom';
 
-function PlaylistCard({PlaylistName, PlaylistUrl, PlaylistPicture, PlaylistOwnerName, PlaylistOwnerLink}) {
+function PlaylistCard({PlaylistName, PlaylistUrl, PlaylistPicture, PlaylistOwnerName, PlaylistOwnerLink, inputDataText}) {
+    // inputDataText = "Your Playlist Results:";
     // console.log('playlist name: ', PlaylistName);
+    
     return (
+        // playlist card containing a picture and two interactable a tags that open the playlist in a new tab
         // <a href={PlaylistUrl} target="_blank">
-            <div className="">
-                {/* <a> */}
-                    <img src={PlaylistPicture} alt="Playlist Image"></img>
-                {/* </a> */}
-                <div>
+            <div className="spotifyPlaylistCard">
+                <div id="spotifyPlaylistCardMiniCard">
                     <a className="" href={PlaylistUrl} target="_blank">
-                        {`Spotify Playlist: ${PlaylistUrl}`}
+                        <img className="spotifyPlaylistPicture" src={PlaylistPicture} alt="Playlist Image"></img>
                     </a>
-                </div>
-                <div>
-                    <a className="" href={PlaylistUrl} target="_blank">
-                        {`Click here for ${PlaylistName}`}
-                    </a>
-                </div>
-                <div>
-                    <h3 className="">
-                        {`Playlist Owner Name: ${PlaylistOwnerName}`}
-                    </h3>
-                </div>
-                <div>
-                    <a className=""href={PlaylistOwnerLink} target="_blank" >
-                        {`Playlist Owner: ${PlaylistOwnerLink}`}
-                    </a>
+                    <div className="playlistTextDiv">
+                        <a className="playlistText" href={PlaylistUrl} target="_blank">
+                            {`Spotify Playlist: ${PlaylistName}`}
+                        </a>
+                    </div>
+                    <div className="playlistTextDiv">
+                        <a className="playlistText" href={PlaylistOwnerLink} target="_blank" >
+                            {`Owner: ${PlaylistOwnerName}`}
+                        </a>
+                    </div>
                 </div>
             </div>
         // </a>
@@ -35,12 +30,26 @@ function PlaylistCard({PlaylistName, PlaylistUrl, PlaylistPicture, PlaylistOwner
 
 export default PlaylistCard;
 
+
+
+
+
+
+
+
+// Use this to make the link open in a separate tab
 // href={PlaylistUrl} target="_blank"
 
+// url for playlist image
+{/* <a>
+    <img src={Playlists/playlists.items[0].images[0].url} alt="Playlist Image"></img>
+</a> */}
 
-
-
-
-
-
-// <a><img src={Playlists/playlists.items[0].images[0].url} alt="Playlist Image"></img></a>
+// instruction div
+{/* <div>
+    <a className="playlistText" href={PlaylistUrl} target="_blank">
+        <h3>
+            {`Click Here For A Playlist That Matches Your Mood`}
+        </h3>
+    </a>
+</div> */}
