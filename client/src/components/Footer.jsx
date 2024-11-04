2c9be5bb14244cfad9ec6a20ad4e7bbb1e811f9a
5mitty <43888178+5mitty@users.noreply.github.com>
2024-11-03 18:44:42 -0600

    added a footer text that includes what api's we are pulling data from and link to their respective api pages, added creator links for all three of us and made the name's interactable to send to respective github profile page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,9 +2,13 @@
 function Footer() {
     return (
         <footer className="">
-            <h4 className="">Meteorologic Melodies</h4>
+            <h4 id='dataSourcesText' className="footerNameLinks">Weather data provided by <a href="https://openweathermap.org/" className="footerNameLinks" target="_blank">OpenWeatherAPI</a> & streaming data provided by <a href="https://developer.spotify.com/documentation/web-api" className="footerNameLinks" target="_blank">SpotifyAPI</a></h4>
+            <h5 className="footerProjectName">Meteorological Melodies</h5>
+            <h5 id='footerCreatorText'>Creators: <a href="https://github.com/dcartolano" className="footerNameLinks" target="_blank">David Cartolano</a>, <a href="https://github.com/5mitty" className="footerNameLinks " target="_blank">Jacob Smith</a>, and <a href="https://github.com/elisabethtracy" className="footerNameLinks" target="_blank">Elisabeth Tracy</a></h5>
         </footer>
     );
 }
 
 export default Footer;
+
+// Meteorological Melodies
